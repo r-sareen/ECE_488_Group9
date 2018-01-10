e45927235af34d2347dedd3a13c01e7c43face99
Rishab Sareen <rishabhsareen@gmail.com>
2018-01-10 18:11:19 -0500

    both designs added for jan 10 class

--- January_10_Class_2.m
+++ January_10_Class_2.m
@@ -2,7 +2,7 @@
 %Conrad Montor - 20460296
 %Adam Sequeira - 20511490
 
-%Design with gain of 500
+%Design with gain of 1000
 
 clear all
 close all
@@ -10,7 +10,7 @@ close all
 s = tf('s');
 P = 1/((s+1)*(s+5)*(s+10));
 
-k = 500;
+k = 1000;
 t_2 = k*0.5/50;
 
 %% Iteration 1
@@ -19,9 +19,9 @@ figure(1);
 bode(k*P);
 
 alpha = (1+sin(deg2rad(50-Pm)))/(1-sin(deg2rad(50-Pm)));
-gain_c = 10*log(alpha);\
+gain_c = 10*log(alpha);
 % Look at plot to find w_c:
-w_c = 2.5;
+w_c = 1.6;
 
 %% Calculate Controller
 t_1 = 1/(sqrt(alpha)*w_c);
