465caf91c17295f118e4ef3b8e16402497046902
Rishab Sareen <rishabhsareen@gmail.com>
2018-01-10 18:06:14 -0500

    Jan 10 class script - PID lead/lag design

---
+++ January_10_Class.m
@@ -0,0 +1,30 @@
+clear all
+close all
+
+s = tf('s');
+P = 1/((s+1)*(s+5)*(s+10));
+
+k = 500;
+t_2 = k*0.5/50;
+
+%% Iteration 1
+[Gm,Pm,Wgm,Wpm] = margin(k*P);
+figure(1);
+bode(k*P);
+
+alpha = (1+sin(deg2rad(50-Pm)))/(1-sin(deg2rad(50-Pm)));
+gain_c = 10*log(alpha);\
+% Look at plot to find w_c:
+w_c = 2.5;
+
+%% Calculate Controller
+t_1 = 1/(sqrt(alpha)*w_c);
+
+z_1 = 1/(alpha*t_1);
+t_d = t_1;
+z_2 = alpha/t_2;
+
+C = k*((s+z_1)*(s+z_2))/(s*(s+1)/t_d);
+figure(2);
+bode(C*P);
+[Gm_comp,Pm_comp,Wgm_comp,Wpm_comp] = margin(C*P);
